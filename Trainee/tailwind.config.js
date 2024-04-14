e51152e61882c342b3e7d7a78bbc8cf25499c997
Hanna Patsenka <98649781+Hannchern@users.noreply.github.com>
2024-04-15 00:38:36 +0200

    Add files via upload

---
+++ Trainee/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+      extend: {
+        screens:{
+          'sm': '640px',
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px',
+          '2xl': '1536px',
+        }
+      },
+      fontFamily:{
+        NanumMyeongjo:['Nanum Myeongjo', 'sans-serif']
+      }
+    },
+    plugins: [],
+  }
